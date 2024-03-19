eb5b8f0885ca845b4ea9044057a112b06feb0e28
Romashka <rroma13579@gmail.com>
2024-03-19 21:16:37 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $name = "Romashka";
+        echo $name;
+
+        //$array = array("Roma", "Kolya", "Anton");
+        //$array = ["Roma", "Kolya", "Anton"];
+        echo $_GET["name"];
+    ?>
+</body>
+</html>
